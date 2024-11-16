7aecb4034439f4793ca5dc9353db9d464284a0be
Pavlo-tr <pavel.topanets@gmail.com>
2024-11-16 16:30:39 +0000

    some changed

--- lecture-01/exercises/01.py
+++ lecture-01/exercises/01.py
@@ -1,3 +1,24 @@
-#
+#first one
 
-print "Alex"
+welcome_text = "Hi, hope you are well!"
+hi = "Hello,"
+hi1 = "!"
+#def - вызов функции 
+def fn():
+    pass 
+fn()
+
+def getName(name):
+    print(name)
+getName ( welcome_text )
+
+name = input("Please enter your name:")
+print(hi,name,hi1)
+
+
+
+
+
+
+    
+
